f1698f7a4cb0a4a1a340720fd1ff787b282a6cbd
Umut Berkay <umutmor7@hotmail.com>
2024-03-04 02:39:17 +0100

    Major Changes + Language Button

--- app.js
+++ app.js
@@ -2,42 +2,45 @@ const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
 const searchResults = document.getElementById("search-results");
 
-// Theme toggler elements
-const themeToggler = document.getElementById("theme-toggler");
+// Language toggler elements
+const languageToggler = document.getElementById("language-toggler");
 const body = document.body;
 
-async function searchWikipeida(query) {
+// Theme toggler elements
+const themeToggler = document.getElementById("theme-toggler");
+
+async function searchWikipedia(query, language) {
   const encodedQuery = encodeURIComponent(query);
-  const endpoint = `https://tr.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${encodedQuery}`;
+  const endpoint = `https://${language}.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${encodedQuery}`;
 
-  const reponse = await fetch(endpoint);
+  const response = await fetch(endpoint);
 
-  if (!reponse.ok) {
-    throw new Error("Faild to fetch search results form wikipedia API.");
+  if (!response.ok) {
+    throw new Error("Failed to fetch search results from Wikipedia API.");
   }
 
-  const json = await reponse.json();
+  const json = await response.json();
   return json;
 }
 
-function displayResults(results) {
+function displayResults(results, language) {
   // Remove the loading spinner
   searchResults.innerHTML = "";
 
   results.forEach((result) => {
-    const url = `https://tr.wikipedia.org/?curid=${result.pageid}`;
+    const url = `https://${language}.wikipedia.org/?curid=${result.pageid}`;
     const titleLink = `<a href="${url}" target="_blank" rel="noopener">${result.title} </a>`;
     const urlLink = `<a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>`;
 
-    const resultItme = document.createElement("div");
-    resultItme.className = "result-item";
-    resultItme.innerHTML = `
+    const resultItem = document.createElement("div");
+    resultItem.className = "result-item";
+    resultItem.innerHTML = `
         <h3 class="result-title">${titleLink}</h3>
         ${urlLink}
         <p class="result-snippet">${result.snippet}</p>
         `;
 
-    searchResults.appendChild(resultItme);
+    searchResults.appendChild(resultItem);
   });
 }
 
@@ -47,30 +50,31 @@ searchForm.addEventListener("submit", async (e) => {
   const query = searchInput.value.trim();
 
   if (!query) {
-    searchResults.innerHTML = "<p>Please enter a valid search term. </p>";
+    searchResults.innerHTML = "<p>Please enter a valid search term.</p>";
     return;
   }
 
-  searchResults.innerHTML = "<div class='spinner'>Loading ... </div>";
+  searchResults.innerHTML = "<div class='spinner'>Loading...</div>";
 
   try {
-    const results = await searchWikipeida(query);
+    const language = body.classList.contains("english") ? "en" : "tr";
+    const results = await searchWikipedia(query, language);
 
     if (results.query.searchinfo.totalhits === 0) {
-      searchResults.innerHTML = "<p>No results found. </p>";
+      searchResults.innerHTML = "<p>No results found.</p>";
     } else {
-      displayResults(results.query.search);
+      displayResults(results.query.search, language);
     }
   } catch (error) {
     console.error(error);
-    searchResults.innerHTML = `<p>An error occured while searching. Please try again later. </p>`;
+    searchResults.innerHTML = "<p>An error occurred while searching. Please try again later.</p>";
   }
 });
 
 // Event listener for the theme toggler
 themeToggler.addEventListener("click", () => {
   body.classList.toggle("dark-theme");
-  if (body.classList.contains("light-theme")) {
+  if (body.classList.contains("dark-theme")) {
     themeToggler.textContent = "Dark";
     themeToggler.style.background = "#fff";
     themeToggler.style.color = "#333";
@@ -78,5 +82,15 @@ themeToggler.addEventListener("click", () => {
     themeToggler.textContent = "Light";
     themeToggler.style.border = "2px solid #ccc";
     themeToggler.style.color = "#333";
+  }
+});
+
+// Event listener for the language toggler
+languageToggler.addEventListener("click", () => {
+  body.classList.toggle("english");
+  if (body.classList.contains("english")) {
+    languageToggler.textContent = "Switch to Turkish";
+  } else {
+    languageToggler.textContent = "Switch to English";
   }
 });
